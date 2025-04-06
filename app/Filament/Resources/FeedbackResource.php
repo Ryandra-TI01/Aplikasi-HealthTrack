88d23b44f726181a62d1ae9c492139ac04f52c7e
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2025-04-06 21:11:24 +0700

    feat(feedback): enable admin access to view feedback resource in Filament

---
+++ app/Filament/Resources/FeedbackResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FeedbackResource\Pages;
+use App\Filament\Resources\FeedbackResource\RelationManagers;
+use App\Models\Feedback;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\Summarizers\Average;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FeedbackResource extends Resource
+{
+    protected static ?string $model = Feedback::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-right';
+    protected static ?string $navigationGroup = 'Solve & Share';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->label('User Name')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\Textarea::make('message')
+                    ->required()
+                    ->columnSpanFull()
+                    ->disabled(),
+                Forms\Components\TextInput::make('rating')
+                    ->required()
+                    ->numeric()
+                    ->default(5)
+                    ->disabled(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('User Name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('user.email')
+                    ->label('Email')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('rating')
+                    ->numeric()
+                    ->summarize(Average::make()->numeric(
+                        decimalPlaces: 0,
+                    ))
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+            ])
+            ->bulkActions([
+                // Tables\Actions\BulkActionGroup::make([
+                    // Tables\Actions\DeleteBulkAction::make(),
+                // ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFeedback::route('/'),
+            'view' => Pages\ViewFeedback::route('/{record}'),
+        ];
+    }
+}
