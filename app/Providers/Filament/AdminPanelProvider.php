42539225bdde3ba257a27724d2b5e62acc83f492
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2025-04-10 10:12:33 +0700

    feat(dashboard):add account profile link in admin dashboard menu

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -58,6 +59,15 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->resources([
                 config('filament-logger.activity_resource')
-            ]);
+            ])
+            ->sidebarFullyCollapsibleOnDesktop()
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label('Profile')
+                    ->url('user/profile')
+                    ->icon('heroicon-s-user-circle')
+                ,
+            ])
+            ;
     }
 }
