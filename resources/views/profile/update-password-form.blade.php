0ebf6a20ede584b9888016379ff23e5f09b1a01e
Ryandra TI-01 <ryandra.athaya1705@gmail.com>
2025-06-05 11:12:49 +0700

    fix(profile):fix submit button issue

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -32,7 +32,7 @@
             {{ __('Saved.') }}
         </x-action-message>
 
-        <x-button>
+        <x-button type="submit" wire:loading.attr="disabled">
             {{ __('Save') }}
         </x-button>
     </x-slot>
