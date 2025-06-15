8d129a1ffd779a8c5e2fa097186591b6636fc24c
Ryandra TI-01 <ryandra.athaya1705@gmail.com>
2025-06-15 11:59:52 +0700

    feat(components): add class link-underline on breadcrumbs button

--- resources/views/livewire/components/breadcrumbs.blade.php
+++ resources/views/livewire/components/breadcrumbs.blade.php
@@ -1,8 +1,8 @@
 <nav class="text-sm text-gray-500 mb-6">
     <ol class="list-reset flex items-center space-x-2">
-        <li>
-            <a href="{{ url('/dashboard') }}" class="text-gray-700 hover:underline flex items-center">
-                <img src="{{ asset('images/home.png') }}" class="h-4 mr-2" alt="home">
+        <li class="flex items-center">
+            <img src="{{ asset('images/home.png') }}" class="h-4 mr-2" alt="home">
+            <a href="{{ url('/dashboard') }}" class="text-gray-700 link-underline">
                 Home
             </a>
         </li>
@@ -14,7 +14,7 @@
                 @if($index === count($items) - 1)
                     <span class="text-primary font-semibold">{{ $item['label'] }}</span>
                 @else
-                    <a href="{{ $item['url'] }}" class="text-primary hover:underline">{{ $item['label'] }}</a>
+                    <a href="{{ $item['url'] }}" class="text-primary link-underline">{{ $item['label'] }}</a>
                 @endif
             </li>
         @endforeach
