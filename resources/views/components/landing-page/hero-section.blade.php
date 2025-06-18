d0be6aa71adf2442343f071b72147452240393c5
Ryandra TI-01 <ryandra.athaya1705@gmail.com>
2025-06-18 19:59:13 +0700

    style(landing page):adjust max-w to fit header and adjust style aos

--- resources/views/components/landing-page/hero-section.blade.php
+++ resources/views/components/landing-page/hero-section.blade.php
@@ -8,19 +8,19 @@
         })
     "
 >
-    <div class="max-w-4xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
+    <div class="max-w-5xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
         <!-- Kiri -->
         <div class="text-center lg:text-left -top-10" data-aos="fade-right">
-            <p class="text-gray-500 mb-2 text-xl" data-aos="fade-up" data-aos-delay="100">
+            <p class="text-gray-500 mb-2 text-xl" data-aos="fade-right" data-aos-delay="100">
                 Welcome to HealthTrack!
             </p>
-            <h1 class="text-3xl md:text-4xl font-bold text-primary leading-tight mb-4" data-aos="fade-up" data-aos-delay="200">
+            <h1 class="text-3xl md:text-4xl font-bold text-primary leading-tight mb-4" data-aos="fade-right" data-aos-delay="200">
                 Reliable Health Tracking <br> for Long–Term Care
             </h1>
-            <p class="text-gray-600 mb-6 max-w-md mx-auto lg:mx-0" data-aos="fade-up" data-aos-delay="300">
+            <p class="text-gray-600 mb-6 max-w-md mx-auto lg:mx-0" data-aos="fade-right" data-aos-delay="300">
                 Manage your medications, schedule and vitals, and connect with a supportive community – all in one trusted platform built with your health in mind.
             </p>
-            <x-button class="transition-all duration-300 hover:scale-105 hover:shadow-lg">
+            <x-button class="transition-all duration-300 hover:scale-105 hover:shadow-lg" data-aos="fade-right" data-aos-delay="400">
                 <a href="{{ route('register') }}">Get Started</a>
             </x-button>
 
